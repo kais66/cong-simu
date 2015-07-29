badab96c72a2da72f9619a588dd2e38b1e2b7b31
Kai Su <kais@winlab.rutgers.edu>
2015-07-29 11:18:01 -0400

    updates

--- driver_script/genDemands.sh
+++ driver_script/genDemands.sh
@@ -8,14 +8,14 @@ small_equal_rates=("0.5" "0.7" "0.9" "1.1" "1.3" "1.5" "2.0")
 # small skewed traffic
 small_skewed_rates=("0.5" "0.7" "0.9" "1.1" "1.3" "1.5" "1.7" "2.0")
 
-# raw sprint traffic
-raw_sprint_rates=()
+# abilene equal traffic
+abilene_equal_rates=("0.45" "0.55" "0.65")
 
 cd ../
 #for (( i=2000; i<30000; i=i+1000)); do
 #    python ./demand.py $i
 #done
 
-for thisRate in "${rates[@]}"; do
-    python ./gen_traffic.py $thisRate
+for thisRate in "${abilene_equal_rates[@]}"; do
+    python ./gen_traffic.py $thisRate AbileneEqual
 done
