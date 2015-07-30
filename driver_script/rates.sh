d52034e423cd8aebd51bd29c2b39da26def5937c
Kai Su <kais@winlab.rutgers.edu>
2015-07-30 15:36:25 -0400

    updates

--- driver_script/rates.sh
+++ driver_script/rates.sh
@@ -6,4 +6,4 @@ small_equal_rates=("0.5" "0.7" "0.9" "1.1" "1.3" "1.5" "2.0")
 small_skewed_rates=("0.5" "0.7" "0.9" "1.1" "1.3" "1.5" "1.7" "2.0")
 
 # abilene equal traffic
-abilene_equal_rates=("0.45" "0.55")
+abilene_equal_rates=("0.05" "0.35" "0.45" "0.55" "0.65")
